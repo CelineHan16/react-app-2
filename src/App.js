d9cb4e95aee66695a7ed54b011b92e8b1bd8855c
syrgo <syrga.v30@gmail.com>
2022-03-23 14:52:11 +0300

    created new component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Paragraph from "./components/Paragraph/Paragraph";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Paragraph size="17px">This is a large sample paragraph</Paragraph>
+      <Paragraph size="14px">This is a sample paragraph</Paragraph>
+      <Paragraph size=".5rem">This is a small sample paragraph</Paragraph>
     </div>
   );
 }
